c922dcf92e3b8859f7b1234f339545c81779d567
Luis André Tolentino Castro <andre_0828@outlook.com>
2020-10-27 17:10:23 -0500

    Se agregan los componentes

---
+++ Backend/Controllers/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Backend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.IO;
+using System.Linq;
+
+namespace Backend.Controllers
+{
+    [Route("api/Game")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        [HttpGet]
+        public Games GetGames(int Id = 0)
+        {
+            Games _Games = new Games();
+            Game _Game = new Game();
+            using (StreamReader read = new StreamReader(@"Util\Game.json"))
+            {
+                string Json = read.ReadToEnd();
+                _Games = JsonConvert.DeserializeObject<Games>(Json);
+            }
+
+            if(Id != 0)
+            {
+                _Game = _Games.GameList.FirstOrDefault(x => x.Id == Id);
+                _Games.GameList.Clear();
+                _Games.GameList.Add(_Game);
+            }
+            
+            return _Games;
+        }
+    }
+}
---
+++ Backend/Models/Games.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Models
+{
+    public class Games
+    {
+        public List<Game> GameList { get; set; }
+        
+    }
+}
